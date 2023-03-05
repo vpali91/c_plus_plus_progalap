e53f992ee44f48a40eaa4d1bf5c52a4c03a9106e
vpali91 <44944542+vpali91@users.noreply.github.com>
2023-03-05 22:27:13 +0100

    Create Switches.cpp

---
+++ documentation/Switches.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //példa számokkal
+    int day = 4;
+    switch (day)
+    {
+    case 1:
+        cout << "Monday" << endl;
+        break;
+    case 2:
+        cout << "Tuesday" << endl;
+        break;
+    case 3:
+        cout << "Wednesday" << endl;
+        break;
+    case 4:
+        cout << "Thursday" << endl;
+        break;
+    case 5:
+        cout << "Friday" << endl;
+        break;
+    case 6:
+        cout << "Saturday" << endl;
+        break;
+    case 7:
+        cout << "Sunday" << endl;
+        break;
+    }
+
+    return 0;
+}
